57f231dc04d6852ccd94add9b0031d6a51abfbd6
Daniel Watkins <daniel.watkins@hogarthww.com>
2013-11-25 10:59:46 +0000

    Add dummy default cache to testsettings.

--- testsettings.py
+++ testsettings.py
@@ -11,3 +11,11 @@ INSTALLED_APPS = (
 
 
 SECRET_KEY = 'abcde12345'
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'reusable-test',
+    }
+}
